d35b2c2c6afa12518a06f3ebfe84b2f0dc1e01a4
meenahoda <meena.hoda@wmfs.net>
2021-06-29 11:04:59 +0100

    feat: generate chef task on insert

--- functions/generate-chef-task.js
+++ functions/generate-chef-task.js
@@ -1,6 +1,27 @@
 module.exports = function () {
   return function generateChefTask (event) {
-    console.log('GENERATE CHEF TASK')
-    console.log(event)
+    const { payload: formData } = event
+
+    const todo = {
+      role: 'pizza_chef',
+      stateMachineTitle: 'Ordered pizza',
+      stateMachineCategory: 'pizza',
+      description: `${formData.name}: ${formData.base || 'N/A'}, ${formData.toppings && formData.toppings.length > 0 ? formData.toppings.join(', ') : 'No Toppings'}`,
+      stateMachineName: 'pizza_viewPizza_1_0',
+      todoTitle: 'Ordered pizza',
+      launches: [
+        {
+          title: 'View Order',
+          stateMachineName: 'pizza_viewPizza_1_0',
+          input: {
+            boardKeys: {
+              pizzaId: formData.id
+            }
+          }
+        }
+      ]
+    }
+
+    return { todo }
   }
 }
